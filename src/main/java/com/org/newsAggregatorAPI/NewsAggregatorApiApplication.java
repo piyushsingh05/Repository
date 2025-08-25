f2ed1feb93e3ae381621ea50c14e4fac91bd3d3c
piyushsingh05 <impiyushkumar5@gmail.com>
2025-08-25 21:57:14 +0530

    t

---
+++ src/main/java/com/org/newsAggregatorAPI/NewsAggregatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.org.newsAggregatorAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAggregatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAggregatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/newsAggregatorAPI/NewsAggregatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.newsAggregatorAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAggregatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
