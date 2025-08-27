4f5fb412d3f9b5f9d1e163f24098fc95d52404d7
piyushsingh05 <impiyushkumar5@gmail.com>
2025-08-27 13:06:11 +0530

    updated

--- src/main/java/com/org/newsAggregatorAPI/entity/User.java
+++ src/main/java/com/org/newsAggregatorAPI/entity/User.java
@@ -1,4 +1,30 @@
 package com.org.newsAggregatorAPI.entity;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+
+    @ElementCollection
+    private List<String> preferences;
+
+    @ElementCollection
+    private Set<String> readArticles = new HashSet<>();
+
+    @ElementCollection
+    private Set<String> favoriteArticles = new HashSet<>();
+
+
 }
